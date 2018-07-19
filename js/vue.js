ba76a26dec8f05ba7bd2b00a466f6624e2b0b69c
BevisT <BevisYu299@163.com>
2018-07-19 16:54:16 +0800

    修改Vue

--- js/vue.js
+++ js/vue.js
@@ -1,11 +1,21 @@
-$(".love").on ("click",function(){
-    alert("赵雅美是美女");
-});
+Vue.component('my-header',{
+    template:'<p>this is my header</p>'
+})
+
+var  myheader = {
+    template:'<P>this is my-header</P>'
+}
 
 var app = new Vue({
     el:'#app',
     data:{
-        message:'Hello Vue.js'
+        message:'Hello Vue.js',
+        fruit:'apple'
+    },  
+    components:{
+        'my-header':myheader
     }
 });
-app.message = 'I am a IT Man';
+app.message = 'I am a IT Man';  
+app.fruit = 'orange';
+
