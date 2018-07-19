0e9682957aea1e586ec9cc4ad866f31a9fbdbaa5
BevisT <BevisYu299@163.com>
2018-07-19 12:18:00 +0800

    初步使用Vue.js

--- js/vue.js
+++ js/vue.js
@@ -1,3 +1,11 @@
 $(".love").on ("click",function(){
     alert("赵雅美是美女");
-})
+});
+
+var app = new Vue({
+    el:'#app',
+    data:{
+        message:'Hello Vue.js'
+    }
+});
+app.message = 'I am a IT Man';
