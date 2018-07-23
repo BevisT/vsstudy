a5df11e1aff38dc12295d595fbcd9bac637ae761
BevisT <BevisYu299@163.com>
2018-07-23 13:40:13 +0800

    Vue的成分使用

--- js/vue.js
+++ js/vue.js
@@ -1,19 +1,19 @@
-Vue.component('my-header',{
-    template:'<p>this is my header</p>'
-})
-
 var  myheader = {
-    template:'<P>this is my-header</P>'
+    template:'<P>this is my-header</P>'  
 }
-
+var mybottom = {
+    template:'<a href="https://www.baidu.com/">宝贝 点我</a>'
+}
+myheader.template='<p>THIS is my header two</p>'
 var app = new Vue({
     el:'#app',
     data:{
         message:'Hello Vue.js',
         fruit:'apple'
     },  
-    components:{
-        'my-header':myheader
+    components:{   
+        'myheader':myheader,
+        'mybottom':mybottom
     }
 });
 app.message = 'I am a IT Man';  
